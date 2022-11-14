15918caab188118db21404b0c1be2bbaa0ae9477
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2022-11-14 20:23:22 +0600

    Implemented counter components with prop usage

--- qwik-app/src/routes/index.tsx
+++ qwik-app/src/routes/index.tsx
@@ -1,10 +1,44 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, setPlatform, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-export default component$(() => {
+export const Counter = component$((props: {step?:number, initial?: number}) => {
+  const state = useStore({
+    count: props.initial
+  });
+  
   return (
     <>
-      <h1>Welcome to Qwik Docs Starter</h1>
+      <span>
+        Hello, Fuad! Here's your counter: {state.count}
+      </span>
+      <div>Times: {state.count}</div>
+      <button
+        className='standard btn'
+        onClick$={ () => {
+          if(state.count){
+            if(props.step){
+              state.count += props.step;
+            } else {
+              state.count++;
+            }
+          } else {
+            state.count = 1;
+          }
+        }}>
+        Increment
+      </button>
+    </>
+  )
+});
+
+export default component$(() => {
+  const state = useStore({
+    count: 0
+  });
+
+  return (
+    <>
+      <h1>Qwik Practise Application</h1>
 
       <ul>
         <li>This homepage uses a layout without a menu.</li>
@@ -28,116 +62,11 @@ export default component$(() => {
         </li>
       </ul>
 
-      <h2>Commands</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href="https://developers.cloudflare.com/pages" target="_blank">
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href="https://expressjs.com/" target="_blank">
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href="https://docs.netlify.com/" target="_blank">
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static</code>
-          </td>
-          <td>
-            <a
-              href="https://qwik.builder.io/qwikcity/static-site-generation/overview/"
-              target="_blank"
-            >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-      </table>
-
-      <h2>Community</h2>
-
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href="https://qwik.builder.io/chat" target="_blank">
-            Chat on discord!
-          </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href="https://twitter.com/QwikDev" target="_blank">
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href="https://github.com/BuilderIO/qwik" target="_blank">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href="https://qwik.builder.io/media/" target="_blank">
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
+      <Counter step={2} initial={10} />
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik Docs Starter',
+  title: 'Qwik Practise App',
 };
